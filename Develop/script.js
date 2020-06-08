8a46997ff0221f3f3c7e484597ac8ff4dcf2b5a0
Ted Pedersen <tedpedersen@me.com>
2020-06-08 12:11:36 -0700

    logic for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,66 @@
-// Assignment code here
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
 
+
+var minPassLength = 8;
+var passLength = 128;
+var promptLength = prompt("How long 8-128 Characters.");
+
+//validation of length 
+if (promptLength > passLength) {
+  alert("Must be less then 128 Charaters!")
+}
+if (promptLength < minPassLength) {
+  alert("Must be greater than 8 Charaters!")
+}
+
+var confirmChar = confirm("Would you like to use special chars?");
+if (confirmChar == true) {
+  var charList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!/#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  console.log(charList);
+  generatePassword()
+} else {
+  var charList = "abcdefghijklmnopqrstuvwxyz";
+  console.log(charList);
+  generatePassword()
+}
+
+
+function generatePassword() {
+  retVal = "";
+  for (var i = 0, n = charList.length; i < promptLength; ++i) {
+      retVal += charList.charAt(Math.floor(Math.random() * n));
+  }
+  console.log(retVal);
+  return retVal;
+}
+
+
+
+// function generatePassword() {
+//   var length = 8,
+//       charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//      
+//       retVal = "";
+//   for (var i = 0, n = charset.length; i < length; ++i) {
+//       retVal += charset.charAt(Math.floor(Math.random() * n));
+//   }
+//   return retVal;
+//   console.log(retVal);
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
